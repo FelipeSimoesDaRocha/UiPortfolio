a6dd98b6c90775e71a1f4b402fbc2e35f478e608
Felipe <Felipe.simoesdarocha@gmail.com>
2023-03-08 17:42:31 -0300

    created _middleware

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "next-auth/client";
+import parseUrl from "../../lib/parseUrl";
+
+async function handle(req: NextRequest, res: NextResponse) {
+  const signInPage = "/login";
+  const errorPage = "/login";
+  const basePath = parseUrl(process.env.NEXTAUTH_URL).path;
+
+  console.log("getSession", getSession)
+
+  if (
+    req.nextUrl.pathname.startsWith(basePath) ||
+    [signInPage, errorPage].includes(req.nextUrl.pathname)
+  ) {
+    return;
+  }
+
+  const requestForNextAuth: any = {
+    headers: {
+      cookie: req.headers.get("cookie"),
+    },
+  };
+
+  const session = await getSession({ req: requestForNextAuth });
+
+  if (session) {
+    return NextResponse.next();
+  }
+
+  const signInUrl = new URL(signInPage, req.nextUrl.origin);
+  return NextResponse.redirect(signInUrl);
+}
+
+export default handle;
